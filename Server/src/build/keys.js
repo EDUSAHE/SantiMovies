41d6776ccc87a14d4e54cdd984b975c0ebfe8985
EDUSAHE <JOSESAHE1@OUTLOOK.COM>
2023-03-28 04:42:36 -0600

    tercera Historia

--- Server/src/build/keys.js
+++ Server/src/build/keys.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'santimovies'
+        host: 'https://databases.000webhost.com/localhost',
+        user: 'id20520436_adminsantimoviesdb',
+        password: 'YYE7=RdzCJ2v?[%9',
+        database: 'id20520436_santimovies'
     }
 };
